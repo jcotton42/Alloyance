721022a981f870b2fe0609ee503609a589a9c1c8
Josh Cotton <jcotton42@outlook.com>
2024-06-30 17:19:56 -0700

    Skeleton.

---
+++ src/main/kotlin/me/jcotton42/deluxemetals/block/ModBlocks.kt
@@ -0,0 +1,18 @@
+package me.jcotton42.deluxemetals.block
+
+import me.jcotton42.deluxemetals.DeluxeMetals
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockBehaviour
+import net.minecraftforge.registries.DeferredRegister
+import net.minecraftforge.registries.ForgeRegistries
+import thedarkcolour.kotlinforforge.forge.registerObject
+
+object ModBlocks {
+    val REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, DeluxeMetals.ID)
+
+    // the returned ObjectHolderDelegate can be used as a property delegate
+    // this is automatically registered by the deferred registry at the correct times
+    val EXAMPLE_BLOCK by REGISTRY.registerObject("example_block") {
+        Block(BlockBehaviour.Properties.of().lightLevel { 15 }.strength(3.0f))
+    }
+}
