ef1ea1ed45ade147f80de4361c130080ab4355e3
Joshua Cotton <jcotton42@outlook.com>
2024-09-13 16:57:13 -0700

    Alloyer recipe generation.

--- src/main/kotlin/me/jcotton42/alloyance/datagen/AlloyanceRecipesProvider.kt
+++ src/main/kotlin/me/jcotton42/alloyance/datagen/AlloyanceRecipesProvider.kt
@@ -1,10 +1,12 @@
 package me.jcotton42.alloyance.datagen
 
 import me.jcotton42.alloyance.Alloyance
+import me.jcotton42.alloyance.datagen.VanillaMetal.*
 import me.jcotton42.alloyance.registration.AlloyanceBlocks
 import me.jcotton42.alloyance.registration.AlloyanceItemTags
 import me.jcotton42.alloyance.registration.AlloyanceItems
 import me.jcotton42.alloyance.registration.Metal
+import me.jcotton42.alloyance.registration.Metal.*
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
 import net.minecraft.data.recipes.*
@@ -16,18 +18,19 @@ import net.minecraft.world.item.Items
 import net.minecraft.world.item.crafting.Ingredient
 import net.minecraft.world.level.ItemLike
 import net.neoforged.neoforge.common.Tags
+import net.neoforged.neoforge.common.crafting.SizedIngredient
 import java.util.concurrent.CompletableFuture
 
 class AlloyanceRecipesProvider(output: PackOutput, lookupProvider: CompletableFuture<HolderLookup.Provider>): RecipeProvider(output, lookupProvider) {
     override fun buildRecipes(output: RecipeOutput) {
         addVanillaCompatRecipes(output)
 
-        // TODO, all the helper methods from vanilla are putting my recipes in the minecraft namespace, need to fix that
         Metal.entries.forEach { metal ->
             addStorageRecipes(output, metal)
             addSmeltingRecipes(output, metal)
             addCrusherRecipes(output, metal)
         }
+        ALLOYS.forEach { addAlloyerRecipes(output, it) }
     }
 
     private fun addVanillaCompatRecipes(output: RecipeOutput) {
@@ -126,6 +129,36 @@ class AlloyanceRecipesProvider(output: PackOutput, lookupProvider: CompletableFu
         }
     }
 
+    private fun addAlloyerRecipes(output: RecipeOutput, alloy: Alloy) {
+        val resultIngot = AlloyanceItems.INGOTS.getValue(alloy.result)
+        val resultDust = AlloyanceItems.DUSTS.getValue(alloy.result)
+        val dustTag1 = getAlloyDust(alloy.input1)
+        val alloyableTag1 = getAlloyAlloyable(alloy.input1)
+        val id1 = getAlloyId(alloy.input1)
+        val dustTag2 = getAlloyDust(alloy.input2)
+        val alloyableTag2 = getAlloyAlloyable(alloy.input2)
+        val id2 = getAlloyId(alloy.input2)
+
+        AlloyerRecipeBuilder(
+            ItemStack(resultIngot.get(), alloy.resultCount),
+            SizedIngredient.of(alloyableTag1, alloy.input1Count),
+            SizedIngredient.of(alloyableTag2, alloy.input2Count),
+            alloy.experience,
+            140)
+            .group(getItemName(resultIngot))
+            .save(output, ResourceLocation.fromNamespaceAndPath(Alloyance.ID, "${getItemName(resultIngot)}_from_alloying"))
+
+        if (alloy.input1Count + alloy.input2Count > 9) return
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, resultIngot, alloy.resultCount)
+            .requires(Ingredient.of(dustTag1), alloy.input1Count)
+            .requires(Ingredient.of(dustTag2), alloy.input2Count)
+            .group(getItemName(resultDust))
+            .unlockedBy("has_${id1}_dust", has(dustTag1))
+            .unlockedBy("has_${id2}_dust", has(dustTag2))
+            .save(output, ResourceLocation.fromNamespaceAndPath(Alloyance.ID, "${getItemName(resultDust)}_from_mixing"))
+    }
+
     private fun crushOre(output: RecipeOutput, dust: ItemLike, oreTag: TagKey<Item>) {
         CrusherRecipeBuilder(ItemStack(dust, 2), Ingredient.of(oreTag), 0.75F, 140)
             .group(getItemName(dust))
@@ -143,4 +176,43 @@ class AlloyanceRecipesProvider(output: PackOutput, lookupProvider: CompletableFu
             .group(getItemName(dust))
             .save(output, ResourceLocation.fromNamespaceAndPath(Alloyance.ID, "${getItemName(dust)}_from_crushing_ingot"))
     }
+
+    private fun getAlloyDust(input: Any): TagKey<Item> = when (input) {
+        is Metal -> AlloyanceItemTags.DUSTS.getValue(input)
+        is VanillaMetal -> input.dustTag
+        else -> throw IllegalArgumentException("Not a vanilla or Alloyance metal: $input")
+    }
+
+    private fun getAlloyAlloyable(input: Any): TagKey<Item> = when (input) {
+        is Metal -> AlloyanceItemTags.ALLOYABLES.getValue(input)
+        is VanillaMetal -> input.alloyableTag
+        else -> throw IllegalArgumentException("Not a vanilla or Alloyance metal: $input")
+    }
+
+    private fun getAlloyId(input: Any): String = when (input) {
+        is Metal -> input.id
+        is VanillaMetal -> input.id
+        else -> throw IllegalArgumentException("Not a vanilla or Alloyance metal: $input")
+    }
 }
+
+private enum class VanillaMetal(val id: String, val dustTag: TagKey<Item>, val alloyableTag: TagKey<Item>) {
+    COPPER("copper", AlloyanceItemTags.DUSTS_COPPER),
+    IRON("iron", AlloyanceItemTags.DUSTS_IRON),
+    GOLD("gold", AlloyanceItemTags.DUSTS_GOLD),
+}
+
+private data class Alloy(
+    val result: Metal,
+    val resultCount: Int,
+    val experience: Float,
+    val input1: Any,
+    val input1Count: Int,
+    val input2: Any,
+    val input2Count: Int,
+    )
+
+private val ALLOYS: List<Alloy> = listOf(
+    Alloy(BRONZE, 4, 1.75f, COPPER, 3, TIN, 1),
+    Alloy(BRASS, 4, 1.75f, COPPER, 3, ZINC, 1),
+)
---
+++ src/main/kotlin/me/jcotton42/alloyance/machine/alloyer/AlloyerBlockEntity.kt
@@ -0,0 +1,8 @@
+package me.jcotton42.alloyance.machine.alloyer
+
+class AlloyerBlockEntity {
+    companion object {
+        // fuel burn times are scaled to this
+        const val NOMINAL_ALLOYING_TIME = 140
+    }
+}
